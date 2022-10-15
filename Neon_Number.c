b74e6464658129375f431977e5881332d2baf79e
21p31a1205 <105282388+21p31a1205@users.noreply.github.com>
2022-10-15 06:54:18 +0530

    #include<stdio.h> int main() {   int n,rem,sq=0,sd=0;   scanf("%d",&n);   sq=n*n;   while(sq!=0)   {     rem=sq%10;     sd=sd+rem;     sq=sq/10;   }   if(sd==n)   {     printf("Neon Number");   }   else   {     printf("Not Neon Number");   } }

--- Neon_Number.c
+++ Neon_Number.c
@@ -1,22 +1,21 @@
-#include<stdio.h>
-int main()
-{
-    int a,b,sum=0,s,i;
-    scanf("%d",&a);
-    s=a*a;
-    for (i=0; s>=1; i++)
-    {
-        b=s%10;
-        sum+=b;
-        s=s/10;
-    }
-    if (sum==a)
-    {
-        printf("Neon Number");
-    }
-    else
-    {
-        printf("Not Neon Number");
-    }
-    return 0;
+#include<stdio.h> 
+int main() 
+{ 
+  int n,rem,sq=0,sd=0;  
+  scanf("%d",&n); 
+  sq=n*n; 
+  while(sq!=0) 
+  {
+    rem=sq%10;
+    sd=sd+rem;
+    sq=sq/10;
+  }
+  if(sd==n)
+  { 
+    printf("Neon Number");
+  }
+  else
+  {
+    printf("Not Neon Number");
+  }
 }
